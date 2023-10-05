35c45f89731a5dbf0c69463ba9ebe15ccb30275f
ludo62 <fournier.ludovic@orange.fr>
2023-10-05 16:41:42 +0200

    ajout du package axios

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -12,6 +12,8 @@ const Auth = () => {
 		setVariant((currentVariant) => (currentVariant === 'login' ? 'register' : 'login'));
 	}, []);
 
+	const register = useCallback(async () => {}, []);
+
 	return (
 		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
 			<div className='bg-black w-full h-full lg:bg-opacity-50'>
