6663e4c094b0f36591feaf9b451d7aab517bc26c
ludo62 <fournier.ludovic@orange.fr>
2023-10-05 16:44:19 +0200

    ajout du fichier pour l'api register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useState } from 'react';
 import Input from '@/components/input';
+import axios from 'axios';
 
 const Auth = () => {
 	const [email, setEmail] = useState('');
@@ -12,7 +13,17 @@ const Auth = () => {
 		setVariant((currentVariant) => (currentVariant === 'login' ? 'register' : 'login'));
 	}, []);
 
-	const register = useCallback(async () => {}, []);
+	const register = useCallback(async () => {
+		try {
+			await axios.post('/api/register', {
+				email,
+				pseudo,
+				password,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
 
 	return (
 		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
