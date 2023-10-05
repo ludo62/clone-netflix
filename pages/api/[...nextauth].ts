9062dda9d406bf776d48fd7b408f58054ca1b453
ludo62 <fournier.ludovic@orange.fr>
2023-10-05 16:39:39 +0200

    configuration de la page Nextauth.ts avec les providers

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,57 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { compare } from 'bcrypt';
+import prismadb from '@/lib/prismadb';
+
+export default NextAuth({
+	providers: [
+		Credentials({
+			id: 'credentials',
+			name: 'Credentials',
+			credentials: {
+				email: {
+					label: 'Email',
+					type: 'text',
+				},
+				password: {
+					label: 'Mot de passe',
+					type: 'passord',
+				},
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error('Veuillez entrer votre email et votre mot de passe');
+				}
+
+				const user = await prismadb.user.findUnique({
+					where: {
+						email: credentials.email,
+					},
+				});
+
+				if (!user || !user.hashedPassword) {
+					throw new Error("Votre email n'est pas enregistré");
+				}
+
+				const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
+				if (!isCorrectPassword) {
+					throw new Error('Mot de passe incorrect');
+				}
+
+				return user;
+			},
+		}),
+	],
+	pages: {
+		signIn: '/connexion',
+	},
+	debug: process.env.NODE_ENV === 'development',
+	session: {
+		strategy: 'jwt',
+	},
+	jwt: {
+		secret: process.env.NEXTAUTH_JWT_SECRET,
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+});
