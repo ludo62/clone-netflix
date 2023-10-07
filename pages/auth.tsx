5d2fcd4d90a49e36eee0a9ad01b5d0bc872efe33
ludo62 <fournier.ludovic@orange.fr>
2023-10-07 12:01:25 +0200

    ajout de la div avec les deux icones pour l'authentification google et github

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,11 @@
+import axios from 'axios';
 import { useCallback, useState } from 'react';
 import Input from '@/components/input';
-import axios from 'axios';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
 	const router = useRouter();
@@ -84,6 +87,14 @@ const Auth = () => {
 						>
 							{variant === 'login' ? 'Se connecter' : 'S’inscrire'}
 						</button>
+						<div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+							<div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+								<FcGoogle size={30} />
+							</div>
+							<div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+								<FaGithub size={30} />
+							</div>
+						</div>
 						<p className='text-neutral-500 mt-12'>
 							{variant === 'login'
 								? "C'est votre pemière visite ?"
